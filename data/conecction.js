5d92776f2a61ab4d03bd7ade05396fb6168cc9c4
Pablo Fernandez <pablofernandez@MacBook-Pro-de-Pablo.local>
2020-06-04 19:26:45 -0300

    variables de entorno

--- data/conecction.js
+++ data/conecction.js
@@ -1,10 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
 //const uri = "mongodb+srv://admin:betp2@cluster0-3bm3a.azure.mongodb.net/test?retryWrites=true&w=majority";
+
 const uri = process.env.MONGODB;
 
 const client = new MongoClient(uri, {useUnifiedTopology: true, useNewUrlParser: true});
 
 async function getConnection(){
+ //   console.log("URi recuperada de .env:" + uri);
     return await client.connect().catch(err => console.error(err));
 }
 
